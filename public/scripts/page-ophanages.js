19733aeef93b5fc5dbc3ccc07dc00e2ae6049ee3
micaellagouveia <micaella2212@gmail.com>
2020-10-13 18:11:56 -0300

    Add map, popup and animations

--- public/scripts/page-ophanages.js
+++ public/scripts/page-ophanages.js
@@ -1,5 +1,27 @@
-const map = L.map('mapid').setView([51.505, -0.09], 13);
+// create map
+const map = L.map("mapid").setView([-15.8113315, -48.0875141], 11);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+// create and add titleLayer
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+// create icon
+const icon = L.icon({
+  iconUrl: "./public/images/map-marker.svg",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+// create popup overlay
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Lar das Meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>'
+);
+
+// create and add marker
+L.marker([-15.8113315, -48.0875141], { icon }).addTo(map).bindPopup(popup);
